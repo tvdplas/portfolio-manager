c21b9264d23582919b76efb87f2644e9b3c8865a
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2021-03-01 12:54:15 +0100

    added server stuff

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+let express = require("express")
+let app = express()
+let path = require("path")
+
+let mysql = require("mysql")
+let con = con = mysql.createConnection({
+    host: "192.168.2.35",
+    user: "crypto",
+    password: "CryptoStonks",
+    database: "portfoliotracker"
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./index.html"))
+})
+
+app.get("/markets/:marketID", (req, res) => {
+    con.connect((err) => {
+        if (err) throw err;
+
+
+        //TODO: THIS INPUT NEEDS TO BE SANITIZED
+        con.query(`SELECT * FROM marketvalue WHERE MarketAbbr = '${req.params.marketID}';`, (qerr, qres) => {
+            res.send(qres.length)
+        })
+    })
+})
+
+
+
+
 const valueUpdating = require('./value-update-job')
