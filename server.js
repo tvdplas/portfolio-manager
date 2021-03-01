d4640ee0a4357fedd3b261b0b4744c4410bea827
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2021-03-01 13:39:32 +0100

    .

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/markets/:marketType/:marketAbbr", (req, res) => {
             let ret = {
                 MarketAbbr: req.params.marketAbbr,
                 MarketType: req.params.marketType,
-                data: JSON.parse(qres)
+                data: qres
             }
             res.send(JSON.stringify(ret))
         })
