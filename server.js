874a803236a31b204ec3694356dc2574fc2dcc91
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2021-03-01 13:36:58 +0100

    .

--- server.js
+++ server.js
@@ -9,18 +9,16 @@ let con = mysql.createConnection({
     password: "CryptoStonks",
     database: "portfoliotracker"
 });
+con.connect((err) => { if (err) throw err })
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./index.html"))
 })
 
 app.get("/markets/:marketType/:marketAbbr", (req, res) => {
-    con.connect((err) => {
-        if (err) throw err;
 
-
-        //TODO: THIS INPUT NEEDS TO BE SANITIZED
-        con.query(`
+    //TODO: THIS INPUT NEEDS TO BE SANITIZED
+    con.query(`
             SELECT DataTime, CurrencyAbbr, Value
             FROM marketvalue 
             WHERE MarketAbbr = '${req.params.marketAbbr}' AND MarketType = '${req.params.marketType}';`,
@@ -28,16 +26,15 @@ app.get("/markets/:marketType/:marketAbbr", (req, res) => {
             let ret = {
                 MarketAbbr: req.params.marketAbbr,
                 MarketType: req.params.marketType,
-                data: qres 
+                data: qres
             }
             res.send(JSON.stringify(ret))
 
             con.end()
         })
-    })
 })
 
-app.listen(7000, () => {console.log("Webserver started on port 7000")})
+app.listen(7000, () => { console.log("Webserver started on port 7000") })
 
 
 const valueUpdating = require('./value-update-job')
