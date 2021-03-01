aa47919fafee990363d0fbfebe80f441bec8a42c
Thomas van der Plas <t.d.vanderplas@students.uu.nl>
2021-03-01 12:26:40 +0100

    moved updating logic to the value update file

---
+++ value-update-job.js
@@ -0,0 +1,59 @@
+const schedule = require("node-schedule")
+const request = require("request")
+let mysql = require("mysql")
+
+var con = mysql.createConnection({
+    host: "192.168.2.35",
+    user: "crypto",
+    password: "CryptoStonks",
+    database: "portfoliotracker"
+});
+
+con.connect((err) => { if (err) throw err })
+
+// Update all market values in the database every minute.
+const job = schedule.scheduleJob("*/1 * * * *", UpdateMarketValues)
+
+function UpdateMarketValues() {
+    let date = new Date().toISOString().slice(0, 19).replace('T', ' ')
+    console.log(`${date} - fetching market data`)
+
+    con.query("SELECT * FROM markets;", (qerr, res) => {
+        if (qerr) throw qerr
+
+        res.forEach(market => {
+            UpdateMarket(market, date)
+        });
+    })
+}
+
+function UpdateMarket(market, date) {
+    //First, get the new value for the market
+    if (market.MarketType = "crypto") {
+        request(`https://api.litebit.eu/market/${market.MarketAbbr}`, (reqErr, res, body) => {
+            if (reqErr) throw reqErr
+
+            let rawMD = JSON.parse(body)
+
+            let MD = {
+                DateTime: date,
+                MarketType: market.MarketType,
+                MarketAbbr: market.MarketAbbr,
+                CurrencyAbbr: "EUR",
+                Value: rawMD.result.sell
+            }
+
+            con.query(`
+                INSERT INTO marketvalue
+                VAlUES ('${MD.DateTime}', '${MD.MarketType}', '${MD.MarketAbbr}', '${MD.CurrencyAbbr}', '${MD.Value}')`,
+                (err, res) => {
+                    if (err) throw err;
+            })
+        })
+    }
+    else {
+        throw new Error("No valid market type found")
+    }
+}
+
+module.exports = job;
