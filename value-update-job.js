d6f2ad9bfb399b75578d489ab238956df154cfa6
Thomas van der Plas <thomasvanderplas.leiden@gmail.com>
2021-03-04 17:38:24 +0100

    .

--- value-update-job.js
+++ value-update-job.js
@@ -92,7 +92,7 @@ function UpdateMarket(market, date) {
         request(`https://finance.yahoo.com/quote/${market.MarketType}?p=${market.MarketType}`, (reqErr, res, body) => {
             if (reqErr) throw reqErr
 
-            console.log($(`.Fw\(b\) .Fz\(36px\) .Mb\(-4px\) .D\(ib\)`, body))
+            console.log($(`.Fw\\(b\\) .Fz\\(36px\\) .Mb\\(-4px\\) .D\\(ib\\)`, body))
         })
     }
     else {
